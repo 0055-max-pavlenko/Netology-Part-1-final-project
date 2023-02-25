cfc08ec2ad4bba0ea50d08e1ee6ecca64c549537
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-02-25 15:17:32 +0300

    Version 2

--- Part_1_final_project.py
+++ Part_1_final_project.py
@@ -1,6 +1,7 @@
 ﻿import os
 import requests
-from datetime import datetime
+import time
+from datetime import datetime, date
 from tqdm import tqdm
 from pprint import pprint
 
@@ -14,7 +15,7 @@ class YaUploader:
             'Accept':'application/json',
             'Authorization':f'OAuth {token}'
             }
-        requests.put(f"{folder_url}?path=Загрузки/VK",headers=headers)
+        requests.put(f"{folder_url}?path=VK_backup_{date.today()}",headers=headers)
     
 
     def _get_upload_link(self, file_path: str):
@@ -44,58 +45,72 @@ class VK:
        self.params = {'access_token': self.token, 'v': self.version}
 
 
-   def get_user_photos(self, owner_id, num_photos = 5):
+   def get_user_photos(self, owner_id, num_photos = 5, offset = 0):
        url = 'https://api.vk.com/method/photos.getAll'
        params = {'owner_id' : owner_id,
                  'extended' :1, 
                  'count': num_photos,
-                 'photo_sizes': 1}
+                 'photo_sizes': 1,
+                 'offset': offset}
        response = requests.get(url, params={**self.params, **params})
        return response.json()
 
-final_list_photos = []
-photo_resolution = 'opqrsmxyzw'
-photo_size ='o'
-photo_url = ''
+def Upload_photos_VK_YandexDrive (owner_backup_id, ya_access_token, vk_access_id, vk_access_token):
 
-owner_id = input('Введите id пользователя vk для загрузки фото:')
-ya_access_token = input('Введите токен для ЯндексДиска:')
-
-with open(r'C:\Users\ASUS\Desktop\Maxim\vk_token.txt', 'r') as file:
-    vk_access_token = file.read().strip()
-
-uploader = YaUploader(ya_access_token)
-
-user_id = '356407569'
-vk = VK(vk_access_token, user_id)
+    final_list_photos = []
+    photo_resolution = 'opqrsmxyzw'
+    photo_size ='o'
+    photo_url = ''
+    
+    
+    vk = VK(vk_access_token, vk_access_id)
+    uploader = YaUploader(ya_access_token)
+    total_number_photos = vk.get_user_photos(owner_backup_id)['response']['count']
+    counter = 0
 
 
+    time.sleep(0.5)
 
-
-print(f'Загружаем данные о фото пользователя {owner_id}')
-for record in tqdm(vk.get_user_photos(owner_id, 200)['response']['items']):
-   file_name = 'likes_'+str(record['likes']['count'])+'_'+str(record['id'])+'_'+datetime.utcfromtimestamp(record['date']).strftime('%Y_%m_%d_%H-%M-%S')+'.jpg' 
-   for size in record['sizes']:
-       if size['type'] in photo_resolution and photo_resolution.index(size['type'])>=photo_resolution.index(photo_size):
-           photo_size = size['type']
-           photo_url = size['url']
-   final_list_photos.append({
-    "file_name": file_name,
-    "size": photo_size,
-    "url": photo_url})
-   photo_size = 'o'
-   photo_url=''
-
-print('\nСоздаем файл с данными о фото')
-with open('photos_info.json', 'wt') as f:
+    print(f'Загружаем данные о фото пользователя {owner_backup_id}')
+    while counter<total_number_photos:
+        for record in vk.get_user_photos(owner_backup_id, 200, counter)['response']['items']:
+            file_name = 'likes_'+str(record['likes']['count'])+'_'+str(record['id'])+'_'+datetime.utcfromtimestamp(record['date']).strftime('%Y_%m_%d_%H-%M-%S')+'.jpg' 
+            for size in record['sizes']:
+                if size['type'] in photo_resolution and photo_resolution.index(size['type'])>=photo_resolution.index(photo_size):
+                    photo_size = size['type']
+                    photo_url = size['url']
+            final_list_photos.append({
+              "file_name": file_name,
+              "size": photo_size,
+              "url": photo_url})
+            photo_size = 'o'
+            photo_url=''
+        counter += 200
+        time.sleep(0.5)
+    print('\nСоздаем файл с данными о фото')
+    with open('photos_info.json', 'wt') as f:
         pprint(final_list_photos, stream=f)
 
 
-print("Загружаем файлы на ЯндексДиск")
-for record in tqdm(final_list_photos):
-    r = requests.get(record['url'])
-    with open('temporary.jpg', 'wb') as temporary_file:
-        temporary_file.write(r.content)
-    uploader.upload_file_to_disk(file_path=f"Загрузки/VK/{record['file_name']}", file_name='temporary.jpg')
+    print("Загружаем файлы на ЯндексДиск")
+    for record in tqdm(final_list_photos):
+        r = requests.get(record['url'])
+        with open('temporary.jpg', 'wb') as temporary_file:
+            temporary_file.write(r.content)
+        uploader.upload_file_to_disk(file_path=f"VK_backup_{date.today()}/{record['file_name']}", file_name='temporary.jpg')
     
-os.remove('temporary.jpg')
+    os.remove('temporary.jpg')
+
+
+
+owner_backup_id = input('Введите id пользователя vk для загрузки фото:')
+ya_access_token = input('Введите токен для ЯндексДиска:')
+print()
+vk_access_id = input('Введите id пользователя, владельца приложения для сохранения фото:')
+vk_access_token = input('Введите токен для vk приложения для сохранения фото:')
+
+Upload_photos_VK_YandexDrive (owner_backup_id, ya_access_token, vk_access_id, vk_access_token)
+
+
+
+
